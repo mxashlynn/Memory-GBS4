53fc59ff7c40f3afbfb4f9bb598f46f58c8a10cd
Paige Ashlynn <mx.ashlynn@gmail.com>
2024-01-11 18:53:45 -0800

    Rearrange tile pattern table
    To support embedding in font files.

--- design/tiles/tile_pattern_table.tsx
+++ design/tiles/tile_pattern_table.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Memory Tiles" tilewidth="8" tileheight="8" tilecount="112" columns="14" backgroundcolor="#65ff00" objectalignment="center">
- <image source="tile_pattern_table.png" trans="65ff00" width="112" height="64"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Memory Tiles" tilewidth="8" tileheight="8" tilecount="138" columns="14" backgroundcolor="#65ff00" objectalignment="center">
+ <image source="tile_pattern_table.png" trans="65ff00" width="128" height="64"/>
 </tileset>
